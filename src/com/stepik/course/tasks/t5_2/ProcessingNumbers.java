68c65a4d54b6ff56e7525db815aaef00723f23ce
Alexey Bliznichenko <l.bliznichenko@mail.ru>
2022-05-24 20:03:51 +0300

    Short-circuit evaluation learnt in streams

--- src/com/stepik/course/tasks/t5_2/ProcessingNumbers.java
+++ src/com/stepik/course/tasks/t5_2/ProcessingNumbers.java
@@ -1,42 +1,42 @@
-package com.stepik.course.tasks.t5_2;
-
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Comparator;
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-public class ProcessingNumbers {
-
-    public static Stream<Integer> getStream(Collection<Integer> numbers) {
-        return switch ((int) (Math.random() * 3)) {
-            case 0 -> numbers.stream()
-                    .sorted()
-                    .takeWhile(number -> number < 100);
-            case 1 -> numbers.stream()
-                    .sorted(Comparator.reverseOrder())
-                    .dropWhile(number -> number >= 100)
-                    .sorted();
-            default -> numbers.stream()
-                    .filter(number -> number < 100)
-                    .sorted();
-        };
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
-                .map(Integer::parseInt)
-                .collect(Collectors.toList());
-
-        String result = getStream(numbers)
-                .map(String::valueOf)
-                .collect(Collectors.joining(" "));
-
-        System.out.println(result);
-    }
-
-}
+//package com.stepik.course.tasks.t5_2;
+//
+//import java.util.Arrays;
+//import java.util.Collection;
+//import java.util.Comparator;
+//import java.util.List;
+//import java.util.Scanner;
+//import java.util.stream.Collectors;
+//import java.util.stream.Stream;
+//
+//public class ProcessingNumbers {
+//
+//    public static Stream<Integer> getStream(Collection<Integer> numbers) {
+//        return switch ((int) (Math.random() * 3)) {
+//            case 0 -> numbers.stream()
+//                    .sorted()
+//                    .takeWhile(number -> number < 100);
+//            case 1 -> numbers.stream()
+//                    .sorted(Comparator.reverseOrder())
+//                    .dropWhile(number -> number >= 100)
+//                    .sorted();
+//            default -> numbers.stream()
+//                    .filter(number -> number < 100)
+//                    .sorted();
+//        };
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        List<Integer> numbers = Arrays.stream(scanner.nextLine().split("\\s+"))
+//                .map(Integer::parseInt)
+//                .collect(Collectors.toList());
+//
+//        String result = getStream(numbers)
+//                .map(String::valueOf)
+//                .collect(Collectors.joining(" "));
+//
+//        System.out.println(result);
+//    }
+//
+//}
--- src/com/stepik/course/tasks/t5_2/SearchJavaEvents.java
+++ src/com/stepik/course/tasks/t5_2/SearchJavaEvents.java
@@ -1,31 +1,31 @@
-package com.stepik.course.tasks.t5_2;
-
-import java.util.List;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-
-public class SearchJavaEvents {
-
-    private static final String JAVA = "java";
-
-    public static void printSortedJavaEvents(List<String> events) {
-        events
-                .stream()
-                .filter(event -> event.toLowerCase().contains("java"))
-                .sorted()
-                .forEach(System.out::println);
-    }
-
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-
-        List<String> events = Stream
-                .iterate(1, i -> scanner.hasNextLine(), i -> i + 1)
-                .map(i -> scanner.nextLine())
-                .collect(Collectors.toList());
-
-        printSortedJavaEvents(events);
-    }
-
-}
+//package com.stepik.course.tasks.t5_2;
+//
+//import java.util.List;
+//import java.util.Scanner;
+//import java.util.stream.Collectors;
+//import java.util.stream.Stream;
+//
+//public class SearchJavaEvents {
+//
+//    private static final String JAVA = "java";
+//
+//    public static void printSortedJavaEvents(List<String> events) {
+//        events
+//                .stream()
+//                .filter(event -> event.toLowerCase().contains("java"))
+//                .sorted()
+//                .forEach(System.out::println);
+//    }
+//
+//    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        List<String> events = Stream
+//                .iterate(1, i -> scanner.hasNextLine(), i -> i + 1)
+//                .map(i -> scanner.nextLine())
+//                .collect(Collectors.toList());
+//
+//        printSortedJavaEvents(events);
+//    }
+//
+//}
